f3cef235b32702ec1258a2f75b68a2722b11048e
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-07-07 22:48:28 -0600

    get results by search

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,36 @@
 <template>
   <q-page class="q-pa-md">
       <q-input
+        v-model="search"
         @keyup.enter="getResults"
-        type="search"
-        placeholder="Ingresa tu equipo"
+        placeholder="Ingresa un nombre de equipo o ciudad"
         >
         <template v-slot:append>
-          <q-icon name="search" @click="getResults" />
+         <q-icon
+            @click="getResults" 
+            name="search"
+          />
         </template>
       </q-input>
+      <div class="col q-pa-md q-mt-md">
+        <div class="text-h6 text-weight-regular">
+          Nombre del estadio: <br> {{stadiumData.name}}
+        </div>
+        <div class="text-h6 text-weight-regular">
+          Capacidad: {{stadiumData.capacity}}
+        </div>
+        <div class="text-h6 text-weight-regular">
+          Ciudad: {{stadiumData.city}}
+        </div>
+        <div class="text-h6 text-weight-regular">
+          Pais: {{stadiumData.country}}
+        </div>
+        <div class="q-mt-md">
+          <q-img
+          :src="stadiumData.image"
+          />
+        </div>
+      </div>
   </q-page>
 </template>
 
@@ -20,33 +42,39 @@ export default defineComponent({
   name: 'IndexPage',
 
   setup () {
+    const search = ref('')
     const apiKey = ref('b6004fba06b499ba6cd2b7b7e00939f5')
     const baseUrl = ref('https://v3.football.api-sports.io')
-    const futbolData = ref(null)
-    
-    const getResults = () => {
-      console.log('GREAT')
-      axios(`${baseUrl.value}/players/squads?team=33`, {
+    const stadiumData = ref('')
+
+    const getResults = async () => {
+      try {
+        console.log('GREAT')
+        const response = await axios(`${baseUrl.value}/venues?search=${search.value}`, {
           method: 'get',
           headers: {
           'x-rapidapi-key': apiKey.value,
           'x-rapidapi-host': 'v3.football.api-sports.io'
         }
       })
-      .then(response => {
-        futbolData.value = response.data
-        console.log(response.data)
-      })
-      .catch((err) => {
+
+        stadiumData.value = response.data.response[0]
+        search.value=''
+        //console.log(response.data.response[0].players)
+        //console.log(response.data.response[0].name)
+        //console.log(stadiumData.value)
+      }
+      catch(err) {
         console.log(err.message)
-      })
-
-
+      }
     }
 
-    return {getResults, futbolData}
+    return {search, getResults, stadiumData}
   }
 
 })
+// Get all players statistics from one {team} & {season}
+//get("https://v3.football.api-sports.io/players?season=2018&team=33");
 
 </script>
+
