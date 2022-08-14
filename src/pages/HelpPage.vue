c42007e61e359e0e61bfc93c8e4a94f96e7132d6
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-08-14 17:40:27 -0600

    recaptcha

--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="q-pa-md">
     <h5 >Help</h5>
-    <p class="text-body1">You can enter the specific name of the stadium then the app will search and show you the specific information of that stadium. </p>
+    <p class="text-body1">You can enter the specific name of the stadium then the app will search and show you the specific information about that stadium. </p>
     <p class="text-body1">You can also enter the name of a city or country that has indeed many stadiums, in that case the app will show you the twelve largest stadiums of that specific city or country. </p>
   </q-page>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,16 +4,30 @@
         v-model="search"
         @keyup.enter="getResults"
         placeholder="Enter the name of the stadium, city or country"
-  
-        standout="bg-blue-grey-10 text-white"
+        standout="dark text-white"
+        :disable="disable"
+   
         >
         <template v-slot:append>
          <q-icon
             @click="getResults" 
             name="search"
+            type="submit"
           />
         </template>
       </q-input>
+
+      <br/>
+
+      <VueRecaptcha
+        class="flex flex-center"
+        :sitekey="siteKey"
+        @verify="handleSuccess"
+        @error="handleError"
+        @expired="handleExpire"
+        ref="recaptcha"
+      ></VueRecaptcha>
+
 
       <div class="row justify-around">
         <div
@@ -70,15 +84,22 @@
   </q-page>
 </template>
 
+
 <script>
 import { defineComponent, ref, computed} from 'vue'
 import axios from 'axios'
 import { useQuasar } from 'quasar'
+import { VueRecaptcha } from 'vue-recaptcha';
 
 export default defineComponent({
   name: 'IndexPage',
+  name: 'ReCaptcha',
+    components: {
+    VueRecaptcha
+  },
 
   setup () {
+
     const $q = useQuasar()
     const search = ref('')
     const apiKey = ref('b6004fba06b499ba6cd2b7b7e00939f5')
@@ -99,22 +120,25 @@ export default defineComponent({
 
         if(stadiumData.value.length === 0){
           search.value=''
-          console.log('Please try to enter the name of the stadium, city or country')
           $q.notify({
-          type: 'negative',
-          message: 'Please try to enter a valid name of a stadium, city or country',
-          position: 'center'
+          icon: 'warning',
+          progress: true,
+          color: 'negative',
+          message: 'Please try to enter a valid name of a stadium, city or country. Thank you!',
+          position: 'center',
           })
         } else {
           stadiumData.value.sort((a,b) => (b.capacity - a.capacity))
-           search.value=''
-           console.log(response.data.response.length)
-           console.log(response.data.response)
-           console.log(stadiumData.value)
+          search.value=''
+
+          console.log(response.data.response.length)
+          console.log(response.data.response)
+          console.log(stadiumData.value)
           console.log(stadiumData.value.slice(0, 12))
           console.log(sortData.value)
 
-          
+          disable.value=true
+          recaptcha.value.reset()
         }
         
       }
@@ -123,27 +147,44 @@ export default defineComponent({
       }
       $q.loading.hide()
     }
+
     const sortData = computed(() => stadiumData.value.slice(0, 12))
+
+/*RECAPTCHA*/
+
+    const siteKey = computed(() => {
+      return '6LfplGYhAAAAACox6XCtc3UjkCSO678toQnzRqsG'
+    })
+    const disable = ref(true)
+    const recaptcha = ref(null)
+    const handleError = () => {
+      console.log('an error ocurred')
+    }
+    const handleSuccess = () => {
+      disable.value = !disable.value
+      console.log('an event ocurred')   
+    }
+    const handleExpire = () => {
+      disable.value = !disable.value
+    }
 
 
     return {
             search, 
             getResults, 
             stadiumData,
-            sortData
+            sortData,
+            siteKey,
+            handleError,
+            handleSuccess,
+            handleExpire,
+            disable,
+            recaptcha,
           }
   }
 
 })
-// Get all players statistics from one {team} & {season}
-//get("https://v3.football.api-sports.io/players?season=2018&team=33");
 
-//// Allows you to search for a venues in relation to a venue {name}, {city} or {country}
-
-//KEEP ALIVE
-//ADD RECAPTCHA
-//DEPLOY
-//TRANSITION OR ANIMATION
 
 </script>
 
